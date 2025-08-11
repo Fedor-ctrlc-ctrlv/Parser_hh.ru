4f1a1b5f65f9b2fcab7268d610bc6b3e0cbdb4a3
Fedor-ctrlc-ctrlv <woodiquiker@gmail.com>
2025-08-11 18:16:56 +0300

    we added a link to the website and defined a structure for storing requests

--- hh.go
+++ hh.go
@@ -4,10 +4,14 @@ import (
 	"github.com/gocolly/colly"
 )
 
+type work struct {
+	wname, money, company string
+}
+
 func main() {
 	c := colly.NewCollector()
 
 	c.UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36"
 
-	c.Visit("")
+	c.Visit("https://hh.ru/search/vacancy?text=&area=1&hhtmFrom=main&hhtmFromLabel=vacancy_search_line")
 }
